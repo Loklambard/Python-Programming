3a4ca1bf299b021d28dcde9f6e64c54bb61c5a5a
Parth Gupta <156999566+Loklambard@users.noreply.github.com>
2024-09-02 20:53:18 +0530

    Add files via upload

---
+++ Basic Program/UI.py
@@ -0,0 +1,16 @@
+#Main program
+while True:
+   print("""what do u want to do
+1.say
+2.say
+3.exit""")
+   choice=int(input("Enter the choice:"))
+   if choice==1:
+       inverted()
+   if choice==2:
+       contain()
+   if choice==3:
+       print("Exited")
+       break
+               
+    
